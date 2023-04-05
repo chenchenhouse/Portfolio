27d41086ed472f950c30c706af969b97b35856a7
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2023-04-05 23:25:14 +0800

    Update WorldChampionshipQual2022_Bot_Forecast.py

--- predictive robot/WorldChampionshipQual2022_Bot_Forecast.py
+++ predictive robot/WorldChampionshipQual2022_Bot_Forecast.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
-
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import os
-import numpy as np
 import datetime
-from time import sleep
-from keras.models import load_model
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-from keras.models import load_model
-import requests
 import json
 import logging
-from requests.auth import HTTPBasicAuth
+import numpy as np
+import os
+import pandas as pd
+import requests
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -30,11 +15,12 @@ class WorldChampionshipQual2022Forecast(object):
     '''
     
     def __init__(self):
-        self.account = ["Geraldine","un77ef8","Reginald7a","Theodoreyyy","zz123zz55","ss11522148","Carlbbomd","ween12235",
-                        "Irving6677","oo12154oo"]
-        self.password = "123123"
+        self.account = [accounts]
+        self.password = password
         self.date = (datetime.datetime.now() + datetime.timedelta(days=7)).strftime("%Y-%m-%d")
-        self.path = r"C:\Users\Guess365User\Bot Forecast\bot_predict_logfile.log"
+        self.path = r"\bot_predict_logfile.log"
+        self.http_account = http_account
+        self.http_password = http_password
         
     def found(self):
         '''
@@ -44,7 +30,7 @@ class WorldChampionshipQual2022Forecast(object):
             print("*********************Found WorldChampionshipQual2022 Data start*********************")   
             try:
                 url = f'https://ecocoapidev1.southeastasia.cloudapp.azure.com/MatchEntryInfo/DateBetween/World Championship Qual 2022/{self.date}~{self.date}'
-                response = requests.get(url,verify=False,auth=HTTPBasicAuth('rick', 'rick000')).text
+                response = requests.get(url,verify=False,auth=HTTPBasicAuth(self.http_account, self.http_password)).text
                 j = json.loads(response)
                 json_data = j['response']
                 data_all = []
@@ -163,7 +149,7 @@ class WorldChampionshipQual2022Forecast(object):
                              'EventCode':value["EventCode"].values[0],
                              'PredictType':'Forecast'}
                             print(data)
-                            response_ = requests.post(url,verify=False, data = data, auth=HTTPBasicAuth('rick', 'rick000')).text
+                            response_ = requests.post(url,verify=False, data = data, auth=HTTPBasicAuth(self.http_account, self.http_password)).text
                             print(response_)
                             count += 1
                             logger.info(f"本日{bot}已預測{data['EventCode']}")
